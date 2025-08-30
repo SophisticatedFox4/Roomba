425bc28555a4e8c9001357f0531c32b9046eb975
SophisticatedFox4 <110247410+SophisticatedFox4@users.noreply.github.com>
2025-08-29 21:32:03 -0700

    Add files via upload

---
+++ RoomCleaner/Driver.java
@@ -0,0 +1,150 @@
+import kareltherobot.*;
+
+public class Driver implements Directions {
+// declared here so it is visible in all the methods!! 
+// It will be assigned a value in the getInfo method
+	private static Robot roomba; 
+	public static void main(String[] args) {
+		// LEAVE THIS ALONE!!!!!!
+		Driver d = new Driver();
+		
+		String wrldName = "TestWorld-2.wld";
+		World.readWorld(wrldName);
+    	World.setVisible(true);
+		World.setDelay(10);
+		
+		int cornerX = 0;
+		int cornerY = 0;
+		int width = 0;
+		int length = 0;
+
+		int totalBeepers = 0;
+		int distance = Integer.MAX_VALUE;
+		int distanceX = 0;
+		int distanceY = 0;
+
+		int maxBeeper = 0;
+		int temp = 0;
+
+		out:
+		for (int i = 2; i < World.numberOfStreets(); i++) {
+			in:
+			for (int j = 2; j < World.numberOfAvenues(); j++) {
+				// scan corners
+				if (World.checkEWWall(i - 1, j) && World.checkNSWall(i, j - 1)) {
+					// get width
+					while (width + i < World.numberOfStreets()) {
+						if (World.checkNSWall(width + i, j - 1)) {
+							if (World.checkEWWall(width + i, j)) {
+								break;
+							} 
+						} else {
+							continue in;
+						}
+						width++;
+					}
+					// get length
+					while (length + j < World.numberOfAvenues()) {
+						if (World.checkEWWall(i - 1, length + j)) {
+							if (World.checkNSWall(i, length + j)) {
+								break;
+							}
+						} else {
+							continue in;
+						}
+						length++;
+					}
+					cornerX = j;
+					cornerY = i;
+					// check opposite length
+					for (int k = 0; k < length; k++) {
+						if (!World.checkEWWall(i + width, j + k)) {
+							continue in;
+						}
+					}
+					// check opposite width
+					for (int k = 0; k < width; k++) {
+						if (!World.checkNSWall(i + k, j + length)) {
+							continue in;
+						}
+					}
+					if (width == 0 || length == 0) {
+						continue in;
+					}
+					break out;
+				}
+			}
+		}
+		
+		Robot roomba = new Robot(cornerY, cornerX, North, 0);
+		
+		// scan total beepers
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < length; j++) {
+				if (World.checkBeeper(cornerY + i, cornerX + j)) {
+					totalBeepers++;
+				}
+			}
+		}
+
+		// get distance
+		while (totalBeepers != 0) {
+			for (int i = 0; i < width; i++) {
+				in:
+				for (int j = 0; j < length; j++) {
+					if (World.checkBeeper(cornerY + i, cornerX + j)) {
+						if (Math.abs(cornerY + i - roomba.street()) + Math.abs(cornerX + j - roomba.avenue()) <= distance) {
+							distance = Math.abs(cornerY + i - roomba.street()) + Math.abs(cornerX + j - roomba.avenue());
+							distanceX = cornerX + j - roomba.avenue();
+							distanceY = cornerY + i - roomba.street();
+						} else {
+							continue in;
+						}
+					}
+				}
+			}
+			if (distanceY > 0) {
+				while (!roomba.facingNorth()) {
+					roomba.turnLeft();
+				}
+			} else {
+				while (!roomba.facingSouth()) {
+					roomba.turnLeft();
+				}
+			}
+			for (int i = 0; i < Math.abs(distanceY); i++) {
+				roomba.move();
+			}
+			if (distanceX > 0) {
+				while (!roomba.facingEast()) {
+					roomba.turnLeft();
+				}
+			} else {
+				while (!roomba.facingWest()) {
+					roomba.turnLeft();
+				}
+			}
+			for (int i = 0; i < Math.abs(distanceX); i++) {
+				roomba.move();
+			}
+			while (World.checkBeeper(roomba.street(), roomba.avenue())) {
+				roomba.pickBeeper();
+				temp++;
+			}
+			if (temp > maxBeeper) {
+				maxBeeper = temp;
+			}
+			temp = 0;
+			distance = Integer.MAX_VALUE;
+			totalBeepers--;
+		}
+		
+    	System.out.println("The biggest pile was " + maxBeeper + " beepers!");
+  	}
+
+	public static void turnRight (Robot r) {
+		for (int i = 0; i < 3; i++) {
+			r.turnLeft();
+		}
+	}	
+}
