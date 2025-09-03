22c4fbec2952fc7aabe079b9e24449dbde4c7904
SophisticatedFox4 <110247410+SophisticatedFox4@users.noreply.github.com>
2025-09-02 17:25:27 -0700

    Update Driver.java

--- RoomCleaner/Driver.java
+++ RoomCleaner/Driver.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 import kareltherobot.*;
 
 public class Driver implements Directions {
@@ -6,10 +8,10 @@ public class Driver implements Directions {
 		Driver d = new Driver();
 		
 		// World stuff
-		String wrldName = "world.wld";
+		String wrldName = "TestWorld-1.wld";
 		World.readWorld(wrldName);
     	World.setVisible(true);
-		World.setDelay(10);
+		World.setDelay(50);
 		
 		// AUTONOMY
 		int cornerX = 0;
@@ -85,12 +87,6 @@ public class Driver implements Directions {
 				}
 			}
 		}
-
-		// last resort
-		if (cornerX == 0 || cornerY == 0) {
-			System.err.println("The code doesn't work. :(");
-			System.exit(1);
-		}
 		
 		Robot roomba = new Robot(cornerY, cornerX, North, 0);
 		
@@ -118,33 +114,46 @@ public class Driver implements Directions {
 					}
 				}
 			}
-			// turn
-			if (distanceY > 0) {
-				while (!roomba.facingNorth()) {
-					roomba.turnLeft();
+			if (Math.signum(distanceX) == 0) {
+				moveY(roomba, distanceY);
+			} else if (Math.signum(distanceY) == 0) {
+				moveX(roomba, distanceX);
+			} else if (Math.signum(distanceX) == Math.signum(distanceY)) {
+				if (distanceX > 0) {
+					if (roomba.facingNorth()) {
+						moveY(roomba, distanceY);
+						moveX(roomba, distanceX);
+					} else {
+						moveX(roomba, distanceX);
+						moveY(roomba, distanceY);
+					}
+				} else {
+					if (roomba.facingSouth()) {
+						moveY(roomba, distanceY);
+						moveX(roomba, distanceX);
+					} else {
+						moveX(roomba, distanceX);
+						moveY(roomba, distanceY);
+					}
 				}
-			} else if (distanceY < 0) {
-				while (!roomba.facingSouth()) {
-					roomba.turnLeft();
+			} else {
+				if (distanceX > 0) {
+					if (roomba.facingEast()) {
+						moveX(roomba, distanceX);
+						moveY(roomba, distanceY);
+					} else {
+						moveY(roomba, distanceY);
+						moveX(roomba, distanceX);
+					}
+				} else {
+					if (roomba.facingWest()) {
+						moveX(roomba, distanceX);
+						moveY(roomba, distanceY);
+					} else {
+						moveY(roomba, distanceY);
+						moveX(roomba, distanceX);
+					}
 				}
-			}
-			// move y-direction
-			for (int i = 0; i < Math.abs(distanceY); i++) {
-				roomba.move();
-			}
-			// turn
-			if (distanceX > 0) {
-				while (!roomba.facingEast()) {
-					roomba.turnLeft();
-				}
-			} else if (distanceX < 0) {
-				while (!roomba.facingWest()) {
-					roomba.turnLeft();
-				}
-			}
-			// move x-direction
-			for (int i = 0; i < Math.abs(distanceX); i++) {
-				roomba.move();
 			}
 			// pick up beepers
 			while (World.checkBeeper(roomba.street(), roomba.avenue())) {
@@ -173,4 +182,38 @@ public class Driver implements Directions {
 		System.out.println("Average pile: " + allBeeper / numBeeper);
 		System.out.println("Percent dirty: " + ((double) numBeeper / ((length + 1) * (width + 1))) * 100 + "%");
   	}
+	
+	public static void moveX (Robot r, int distanceX) {
+		// turn
+		if (distanceX > 0) {
+			while (!r.facingEast()) {
+				r.turnLeft();
+			}
+		} else if (distanceX < 0) {
+			while (!r.facingWest()) {
+				r.turnLeft();
+			}
+		}
+		// move
+		for (int i = 0; i < Math.abs(distanceX); i++) {
+			r.move();
+		}
+	}
+
+	public static void moveY (Robot r, int distanceY) {
+		// turn
+		if (distanceY > 0) {
+			while (!r.facingNorth()) {
+				r.turnLeft();
+			}
+		} else if (distanceY < 0) {
+			while (!r.facingSouth()) {
+				r.turnLeft();
+			}
+		}
+		// move
+		for (int i = 0; i < Math.abs(distanceY); i++) {
+			r.move();
+		}
+	}
 }
