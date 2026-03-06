0f960e078ab8f8e9d3860e0e515e3afb7bef0b9e
SophisticatedFox4 <110247410+SophisticatedFox4@users.noreply.github.com>
2026-03-06 07:34:37 -0800

    Add files via upload

---
+++ intPair.java
@@ -0,0 +1,14 @@
+public class intPair {
+    private int x, y;
+
+    public intPair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+}
